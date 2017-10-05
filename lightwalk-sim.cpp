a2b4301522d11a6b0e8ea948ed2bcc3cd3ffb782
Brent Neal Reeves <brent@twinbear.com>
2017-10-04 22:10:39 -0500

    Rainbow static interactive

--- lightwalk-sim.cpp
+++ lightwalk-sim.cpp
@@ -62,7 +62,8 @@ const int WINDOW_WIDTH = SIM_NODES * NODEWIDTH;
 // #include "lib/Bubbles.cpp"
 // #include "lib/Magneto.cpp"
 // #include "lib/WhackAMole.cpp"
-#include "lib/Shimmer.cpp"
+// #include "lib/Shimmer.cpp"
+#include "lib/Rainbow.cpp"
 
 // =============================================================================================
 // main
@@ -230,9 +231,10 @@ int main(int argc, char *argv[])
     for (int ee = 0; ee < SIM_NODES; ee++)
     {
         // Bubbles *e = new Bubbles(ee, 0, 200, 0,200);
-        Shimmer *e = new Shimmer(ee, 0, rgb_r, rgb_g, rgb_b);
+        // Shimmer *e = new Shimmer(ee, 0, rgb_r, rgb_g, rgb_b);
         // AcidRain *e = new AcidRain(ee, 0, rgb_r, rgb_g, rgb_b, speed);
         // RainbowRain *e = new RainbowRain(ee, 0);
+        Rainbow *e = new Rainbow(ee,0);
         // SplishSplash e = new SplishSplash(0, rgb_r, rgb_g, rgb_b, ee * 30, speed);
 
         // Magneto *e = new Magneto(ee, 0, rgb_r, rgb_g, rgb_b, speed);
