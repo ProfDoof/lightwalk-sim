8e37d32cac9a77c11b61511e6b1261b85fbb172e
Cole Spears <colespears@Coles-MacBook-Pro.local>
2017-10-08 12:49:22 -0500

    Fixed some tech debt

--- lib/TugOfWar.cpp
+++ lib/TugOfWar.cpp
@@ -19,6 +19,10 @@ public:
     _isGoing = true;
     startTime = st;
     teamWinner = 0; //No winner yet
+
+    blinkCount = 0; //reset values for win condition
+    countThreshold = 300;
+    maxThreshold = 1500;
   }
 
   void endGame() {
@@ -36,6 +40,9 @@ public:
   uint32_t _teamOneColor = _gmRGBToColor(0, 0, 255);
   uint32_t _teamTwoColor = _gmRGBToColor(255, 0, 0);
   int teamWinner;
+  int blinkCount;
+  int countThreshold;
+  int maxThreshold;
 
 private:
   uint32_t _gmRGBToColor(int r, int g, int b) {
@@ -59,7 +66,7 @@ public:
     if (_gameManager._isGoing)
       return true;
     else {
-      if (count > threshold/2){  //if count is divisble by 500
+      if (_gameManager.blinkCount > _gameManager.countThreshold/2){  //if count is divisble by 500
         return true;
       }
     }
@@ -82,24 +89,23 @@ private:
   void _update() {
     //check end game case
     if (_gameManager._teamOnePercent <= .05){ //least xOffset value percent
-      _gameManager._isGoing = false; //teamOne wins
+      _gameManager._isGoing = false;
       _gameManager.teamWinner = 1;
     }
     else if (_gameManager._teamOnePercent > .95){ //greatest xOffset value perecent
-      _gameManager._isGoing = false; //teamTwo wins
+      _gameManager._isGoing = false;
       _gameManager.teamWinner = 2;
     }
 
     if (_gameManager.teamWinner){ //if someone has won
 
-      if (count > threshold)
-        count = 0;
-      count++;
-      maxThreshold--;
+      if (_gameManager.blinkCount > _gameManager.countThreshold)
+        _gameManager.blinkCount = 0;
 
-      if (maxThreshold == 0){
-        count = 0;
-        maxThreshold = 500; 
+      _gameManager.blinkCount++;
+      _gameManager.maxThreshold--;
+
+      if (_gameManager.maxThreshold == 0){
         _gameManager.startGame(_startTime);
       }
     }
@@ -108,23 +114,13 @@ private:
   void _movementOn(long currentTime, int xOffset, int nodeId) {
     float localPercent = ((xOffset + diff/2) * 1.0) / (NODE_COUNT * REED_COUNT);
 
-      // move perecent based upon realtime percents (teams have to move to fit their color)
-    if (localPercent < _gameManager._teamOnePercent)
+    // move perecent based upon realtime percents (teams have to move to fit their color)
+    if (localPercent < _gameManager._teamOnePercent)  //(localPercent <= .5) if want to split half-way
       _gameManager._teamOnePercent = _gameManager._teamOnePercent - .025;
     else
       _gameManager._teamOnePercent = _gameManager._teamOnePercent + .025;
 
-    //  move percent based upon half-way point (teams do not have to move fit their color)
-    // if (localPercent <= .5){
-    //   _gameManager._teamOnePercent = _gameManager._teamOnePercent - .025;
-    //   cout << "Here 1" << endl;
-    // }
-    // else {
-    //   _gameManager._teamOnePercent = _gameManager._teamOnePercent + .025;
-    //   cout << "Here 2" << endl;
-    // }
-
-    // must "reset" _activeMovements back to 0. _movementOn returns is _activeMovements = NODE_COUNT"
+    // must "reset" _activeMovements back to 0. _movementOn returns if _activeMovements == NODE_COUNT
     _activeMovements -= 1;
   }
 
@@ -134,7 +130,4 @@ private:
   GameManager _gameManager;
   int diff = 12;
   long _startTime;
-  int count = 0;
-  int threshold = 250;
-  int maxThreshold = 1500;
 };
